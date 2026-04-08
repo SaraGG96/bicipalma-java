eb7b6eff01ec2bdfcfca6a9046bda23be536f68f
Sara <saragarciagarrido96@gmail.com>
2026-04-08 10:03:43 +0200

    feat: Inicializar Anclajes

--- src/main/java/edu/teamrocket/bicipalma/Anclajes.java
+++ src/main/java/edu/teamrocket/bicipalma/Anclajes.java
@@ -1,5 +1,49 @@
 package edu.teamrocket.bicipalma;
 
 public class Anclajes {
-    
+    private final Anclaje[] anclajes;
+
+    public Anclajes(int numAnclajes) {
+        this.anclajes = new Anclaje[numAnclajes];
+        crearAnclajes();
+    }
+
+    private void crearAnclajes() {
+        for (int i = 0; i < anclajes.length; i++) {
+            anclajes[i] = new Anclaje();
+        }
+    }
+
+    public Anclaje[] anclajes() {
+        return this.anclajes;
+    }
+
+    public int numAnclajes() {
+        return this.anclajes.length;
+    }
+
+    public void ocuparAnclaje(int posicion, Movil bici) {
+        this.anclajes[posicion].anclarBici(bici);
+    }
+
+    public boolean isAnclajeOcupado(int posicion) {
+        return this.anclajes[posicion].isOcupado();
+    }
+
+    public void liberarAnclaje(int posicion) {
+        this.anclajes[posicion].liberarBici();
+    }
+
+    public Movil getBiciAt(int posicion) {
+        return this.anclajes[posicion].getBici();
+    }
+
+    public int seleccionarAnclaje() {
+        for (int i = 0; i < anclajes.length; i++) {
+            if (!anclajes[i].isOcupado()) {
+                return i;
+            }
+        }
+        return -1; 
+    }
 }
