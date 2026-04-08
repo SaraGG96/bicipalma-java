fa2c5ea3955fc3d0609265a7ac9ef63c10850cf2
Sara <saragarciagarrido96@gmail.com>
2026-04-08 10:02:30 +0200

    feat: Inicializar clase Bicicleta

--- src/main/java/edu/teamrocket/bicipalma/Bicicleta.java
+++ src/main/java/edu/teamrocket/bicipalma/Bicicleta.java
@@ -1,5 +1,19 @@
 package edu.teamrocket.bicipalma;
 
-public class Bicicleta {
-    
+public class Bicicleta implements Movil {
+    private int id;
+
+    public Bicicleta(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(id);
+    }
 }
