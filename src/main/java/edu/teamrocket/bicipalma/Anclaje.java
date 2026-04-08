9a4ddc7d14d120594390c778b60af843338cf266
Sara <saragarciagarrido96@gmail.com>
2026-04-08 10:03:02 +0200

    feat: Inicializar Anclaje

--- src/main/java/edu/teamrocket/bicipalma/Anclaje.java
+++ src/main/java/edu/teamrocket/bicipalma/Anclaje.java
@@ -1,5 +1,31 @@
 package edu.teamrocket.bicipalma;
 
 public class Anclaje {
-    
+    private boolean ocupado = false;
+    private Movil bici = null;
+
+    public Anclaje() {}
+
+    public boolean isOcupado() {
+        return this.ocupado;
+    }
+
+    public Movil getBici() {
+        return this.bici;
+    }
+
+    public void anclarBici(Movil bici) {
+        this.bici = bici;
+        this.ocupado = true;
+    }
+
+    public void liberarBici() {
+        this.bici = null;
+        this.ocupado = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Ocupado: " + ocupado;
+    }
 }
