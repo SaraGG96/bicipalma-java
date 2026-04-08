d874f6e27b626ee262cfa301282be5f2065ae1b5
Sara <saragarciagarrido96@gmail.com>
2026-04-08 10:02:03 +0200

    feat: Inicializar TarjetaUsuario

--- src/main/java/edu/teamrocket/bicipalma/TarjetaUsuario.java
+++ src/main/java/edu/teamrocket/bicipalma/TarjetaUsuario.java
@@ -1,7 +1,25 @@
 package edu.teamrocket.bicipalma;
 
-public class TarjetaUsuario {
-    
+public class TarjetaUsuario implements Autenticacion {
+    private String id;
+    private boolean activada;
 
+    public TarjetaUsuario(String id, boolean activada) {
+        this.id = id;
+        this.activada = activada;
+    }
 
+    @Override
+    public boolean isActivada() {
+        return this.activada;
+    }
+
+    public void setActivada(boolean activada) {
+        this.activada = activada;
+    }
+
+    @Override
+    public String toString() {
+        return "ID Tarjeta: " + id + " | Activada: " + activada;
+    }
 }
